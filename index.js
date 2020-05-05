a0b57fb26d9a7ac0a4e178d79d943e7017663462
RomaRomeo <r.pidluzhnyi@ukr.net>
2020-05-05 15:42:38 +0300

    simple calculator function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const pressButton = document.getElementById('press'),
-pushNumbers = document.getElementsByClassName('number');
+const pressButton = document.getElementById('press');
+const pushNumbers = document.getElementsByClassName('number');
 pressButton.addEventListener('click', addNumbers);
 
 function addNumbers() {
 	let result = 0;
-	for (let i = 0; i < pushNumbers.length; i++) {
-		result += +pushNumbers[i].value;
+	for (let i = 0; i < Number(pushNumbers.length); i++) {
+		result += Number(pushNumbers[i].value);
 	}
 	const numberResult = document.getElementById('result');
 	numberResult.value = result;
